c13a2046334ca6b4707766edcfde8625969b6990
a656184 <eric.l.kristensen@gmail.com>
2013-08-15 23:15:04 -0700

    final commit

--- application.js
+++ application.js
@@ -1,13 +1,46 @@
+function Dice() {
+  this.side_facing = 0;
+}
+
+Dice.prototype.roll = function () {
+  this.side_facing = Math.floor((Math.random()*6)+1);
+};
+
+function Board() {
+  this.dice = [];
+}
+
+Board.prototype.addDice = function() {
+  this.dice.push(new Dice());
+};
+
+Board.prototype.renderNewDice = function() {
+  $('.dice').append("<div class='die'>0</div>");
+};
+
+Board.prototype.rollAllDice = function() {
+  for (var i = 0; i < this.dice.length; i++){
+    console.log(this.dice[i].roll());
+  }
+};
+
+Board.prototype.renderAfterRoll = function() {
+  $(this.dice).each(function(index, element){
+    $(".dice .die:nth-child(" + (index + 1) + ")").text(element.side_facing);
+  });
+};
+
+
+
 $(document).ready(function() {
-  $('#roller button.add').on('click', function() {
-    console.log("WAT")
-    $('.dice').append('<div class="die">0</div>');
+  game = new Board;
+  $('.add').on('click', function() {
+    game.addDice();
+    game.renderNewDice();
   });
 
-  $('#roller button.roll').on('click', function() {
-    $('.die').each(function(k, die) {
-      var value = Math.floor((Math.random()*6)+1);
-      $(die).text(value);
-    });
+  $('.roll').on('click', function() {
+    game.rollAllDice();
+    game.renderAfterRoll();
   });
 });
